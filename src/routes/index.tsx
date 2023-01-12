c5eacf529ae054a5d4ca2091c356768519159a61
Daniela Bravo <therealdanie@protonmail.com>
2023-01-12 17:11:22 -0600

    feat: #3 Adds routing to project

---
+++ src/routes/index.tsx
@@ -0,0 +1,31 @@
+import {RouteObject} from 'react-router-dom'
+import {Home, Contact, About, Blog, Post, CreatePost} from '../views'
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/contact',
+    element: <Contact/>
+  },
+  {
+    path: '/about',
+    element: <About/>
+  },
+  {
+    path: '/blog',
+    element: <Blog/>
+  },
+  {
+    path: '/post/:id',
+    element: <Post/>
+  },
+  {
+    path: '/new',
+    element: <CreatePost/>
+  }
+]
+
+export default routes
---
+++ src/views/Post.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {useParams} from 'react-router-dom'
+
+function Post (): React.ReactElement<{}> {
+    const {id} = useParams()
+    
+    return (<>{`Post: ${id}`}</>)
+}
+
+export default Post
