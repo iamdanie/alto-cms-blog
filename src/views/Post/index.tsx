c0a5a8c355a7a89dc3ee383b6b2687369a43b183
Daniela Bravo <therealdanie@protonmail.com>
2023-01-13 12:23:31 -0600

    feat: #9 WIP - post UI view

--- src/views/Post/index.tsx
+++ src/views/Post/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { getPostById } from '../../store/post'
+import type { Post } from '../../types/Post'
 
-function Post(): React.ReactElement<{}> {
+function PostView(): React.ReactElement<{}> {
   const { id } = useParams()
+  const [post, setPost] = React.useState<Post>()
 
-  return <>{`Post: ${id as string}`}</>
+  React.useEffect(() => {
+    if (id && !Number.isNaN(id)) {
+      const post = getPostById(id)
+
+      setPost(post)
+    }
+  }, [id])
+
+  if (!post) {
+    return <div>Post Not Found</div>
+  }
+
+  return (<><div>{post.title}</div> <div dangerouslySetInnerHTML={{ __html: post.content }} /></>)
 }
 
-export default Post
+export default PostView
