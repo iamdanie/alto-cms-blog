e210e1e02461e8580bb112d2096f0e2866b05c5b
Daniela Elizabeth Bravo Baez <therealdanie@protonmail.com>
2023-01-14 18:54:32 -0600

    feat: #12 WIP - Implemented cta search

--- src/components/TopMenu/components/SearchBar/index.tsx
+++ src/components/TopMenu/components/SearchBar/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import Modal from '../../Modal'
+import Modal from '../../../Modal'
+import { RiSearchLine } from 'react-icons/ri'
+import './index.css'
 
 function SearchBar(): React.ReactElement<{}> {
   const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false)
 
   return (
     <>
-      <div onClick={() => setIsModalOpen(true)}>SearchBar</div>
+      <div className="search-action" onClick={() => setIsModalOpen(true)}>
+        <RiSearchLine />
+        <div>Search</div>
+      </div>
       <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <div style={{ color: 'black', fontWeight: 600 }}>{'Search Here'}</div>
       </Modal>
